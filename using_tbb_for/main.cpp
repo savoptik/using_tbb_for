7f24938383b401cdd225270d56afb7e3bb0d5785
savoptik <sav70792@gmail.com>
2018-12-15 00:04:40 +0300

    Initial Commit

---
+++ using_tbb_for/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  using_tbb_for
+//
+//  Created by Артём Семёнов on 15/12/2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
